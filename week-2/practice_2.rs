3e42763c081408ea53d84f0bc1ad9bd5c6f3c632
cid kaganou <eyiyemi.abidogun@pau.edu.ng>
2026-09-02 22:58:46 +0100

    (My second commit)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,6 @@
+fn main() {
+	println!();// good day sir i hope your day is going good
+	println!("hello");
+	println!("format {} arguments","some" );
+	println!("My Name is {}. I am the first in my class","Eyiyemi Abidogun" );
+}
---
+++ week-2/practice_3.rs
@@ -0,0 +1,5 @@
+fn main() {
+	let fees = 29_001;
+	let salary:i64 = 30_000;
+	println!("fee is {} and salary is {}",fees,salary );
+}
